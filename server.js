598bba77dc5a964a43b10e564cc578d9e0817c7c
tnguyen303 <tringuyen552911@gmail.com>
2018-11-27 17:31:33 -0500

    working sql

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const bodyParser = require("body-parser");
+const { Client } = require('pg');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,13 +16,6 @@ app.use(express.static("public"));
 
 // parse application/json
 app.use(bodyParser.json());
-
-// Connect to the Mongo DB through mlab
-// mongoose.connect(
-//   // process.env.MONGODB_URI ||
-//   "mongodb://user:123456a@ds121203.mlab.com:21203/heroku_zr357z2k",
-//   { useNewUrlParser: true }
-// );
 
 require("./sockets/todolist-sockets")(io);
 require("./routes/html-routes")(app);
@@ -36,4 +30,22 @@ db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+});
+
+const client = new Client({
+  connectionString: "postgres://nawejhzthcupab:514297fa0de16a12e47c4f58e7b1fb5831237317b12849844499e81d537b4381@ec2-23-21-201-12.compute-1.amazonaws.com:5432/dbfm99geb942eu" 
+  // ||
+  // "dbname=dbfm99geb942eu host=ec2-23-21-201-12.compute-1.amazonaws.com port=5432 user=nawejhzthcupab password=514297fa0de16a12e47c4f58e7b1fb5831237317b12849844499e81d537b4381 sslmode=require"
+  ,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
