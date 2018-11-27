579e6ef0e31a4f464ee30315645b3f7e413023cd
tnguyen303 <tringuyen552911@gmail.com>
2018-11-27 17:01:27 -0500

    added mysql

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
-const moment = require('moment');
+const db = require("./models");
+const bodyParser = require("body-parser");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// parse application/json
+app.use(bodyParser.json());
+
 // Connect to the Mongo DB through mlab
-mongoose.connect(
-  // process.env.MONGODB_URI ||
-  "mongodb://user:123456a@ds121203.mlab.com:21203/heroku_zr357z2k",
-  { useNewUrlParser: true }
-);
+// mongoose.connect(
+//   // process.env.MONGODB_URI ||
+//   "mongodb://user:123456a@ds121203.mlab.com:21203/heroku_zr357z2k",
+//   { useNewUrlParser: true }
+// );
 
 require("./sockets/todolist-sockets")(io);
 require("./routes/html-routes")(app);
@@ -25,4 +29,11 @@ require("./routes/api-routes")(app);
 
 server.listen(PORT, function() {
   console.log(`Server listening on PORT ${PORT}`);
+});
+
+// Starts the server to begin listening
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
